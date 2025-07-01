6952023ac0bd5c214e858ff0cabc07a0e71e99a1
boazFridenberg <boazfree152@gmail.com>
2025-07-01 12:24:28 +0300

    add env dev key to mcp.json

--- src/safe.ts
+++ src/safe.ts
@@ -1,3 +1,2 @@
-export const devkey: string = "sQ84wpdxRTR4RMCaE9YqS4";
 export const version: string = "AppsFlyer_6.13.0";
 //There are several versions of the APPSFLYER SDK, so you need to put the version you are using here.
